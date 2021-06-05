e92e6d5ce169fa949c038c3cda05db9d616355f9
trish <pmgriego@gmail.com>
2021-06-04 22:33:32 -0700

    create routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,55 @@
-from flask import Blueprint
+import re
+from app import db
+from app.models.book import Book
+from flask import request, Blueprint, make_response, jsonify
 
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@books_bp.route("", methods=["GET","POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json(request.data)
+        new_book = Book(
+            title=request_body["title"],
+            description=request_body["description"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return make_response(f"Book {new_book.title} successfully created.", 201)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+@hello_world_bp.route('/hello-world', methods=["GET"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello World"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello-world/JSON", methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "Trish",
+        "message": "It does not do well to dwell on dreams and forget to live.",
+        "hobbies": [
+            "Fishing",
+            "Swimming",
+            "Hanging out with Chickens"
+        ]
+    }
