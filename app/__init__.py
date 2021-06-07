4d7c5da43756229291f37c40ce84a38ba4c43d12
trish <pmgriego@gmail.com>
2021-06-06 20:30:41 -0700

    clean up and switch in env vars for db

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,35 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI"
+        )
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI"
+        )
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import hello_world_bp
+    # from app.models.book import Book
+    # do I need this?
+    
     from .routes import books_bp
-
-    # from app.models.book import books_bp
-
     app.register_blueprint(books_bp)
-    app.register_blueprint(hello_world_bp)
 
     return app
