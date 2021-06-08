358290940a625ccf11d8ee8616eecb4daa1566d7
trish <pmgriego@gmail.com>
2021-06-08 01:06:38 -0700

    add whitespace for better readability

--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,12 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI"
         )
-
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
